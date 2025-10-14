8b77a7e60d11eeefc4556c73d6d76f2d6580b994
Grace Amelia <166779102+graceameliaa@users.noreply.github.com>
2025-10-14 19:12:19 +0700

    Delete View.sql

--- View.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW view_ChurnData AS
-	SELECT * FROM prod_Churn
-		WHERE Customer_Status IN ('Churned', 'Stayed')
-
-CREATE VIEW view_JoinData AS
-	SELECT * FROM prod_Churn
-		WHERE Customer_Status IN ('Joined')
